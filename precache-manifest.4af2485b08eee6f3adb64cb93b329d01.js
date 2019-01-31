7b8d8390670850c509a2020f79fb8835f6974d49
vyeung <vl.yeung@yahoo.com>
2019-01-30 22:46:44 -0500

    Updates

---
+++ precache-manifest.4af2485b08eee6f3adb64cb93b329d01.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "24018428f4bb3c18de949f8edfbf7fe3",
+    "url": "/ReactJS-Panda-Day/static/media/slide3.24018428.jpg"
+  },
+  {
+    "revision": "6332fe77d7d7f6bf13a9",
+    "url": "/ReactJS-Panda-Day/static/css/main.32b41798.chunk.css"
+  },
+  {
+    "revision": "bfdcd964031e6630bce6",
+    "url": "/ReactJS-Panda-Day/static/js/1.bfdcd964.chunk.js"
+  },
+  {
+    "revision": "18acd50c729fed40a686",
+    "url": "/ReactJS-Panda-Day/static/js/runtime~main.18acd50c.js"
+  },
+  {
+    "revision": "69a5ccd5227a21ef984b8bfb7863783e",
+    "url": "/ReactJS-Panda-Day/static/media/slide2.69a5ccd5.jpg"
+  },
+  {
+    "revision": "6332fe77d7d7f6bf13a9",
+    "url": "/ReactJS-Panda-Day/static/js/main.6332fe77.chunk.js"
+  },
+  {
+    "revision": "d7ac0df90785e3781c5b2cb621136d8f",
+    "url": "/ReactJS-Panda-Day/static/media/slide1.d7ac0df9.jpg"
+  },
+  {
+    "revision": "f28ba39505614047e3fb179b322f5936",
+    "url": "/ReactJS-Panda-Day/static/media/mug.f28ba395.jpg"
+  },
+  {
+    "revision": "4f772db3b740349c6a124cc5e06c9e7e",
+    "url": "/ReactJS-Panda-Day/static/media/shirt.4f772db3.jpg"
+  },
+  {
+    "revision": "0ff0144396217cbbcf979b10dec0d00c",
+    "url": "/ReactJS-Panda-Day/static/media/plush.0ff01443.jpg"
+  },
+  {
+    "revision": "af80ffaec098b71aea8bf5b324c5fd7c",
+    "url": "/ReactJS-Panda-Day/index.html"
+  }
+];
